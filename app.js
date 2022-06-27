b20286de8c772c515b303c081b3262e5ca3c24ce
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-06-27 00:34:10 -0400

    updated project

---
+++ app.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//configure creating mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password1234'
+})
+
+//Executing Connection
+connection.connect((err) => {
+    if (err) {
+        return console.error('error' + err.message)
+    } console.log('Connected to the MySQL server.')
+})
+
+//static file
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+// app.use('/css', express.static(__dirname + 'public/css'))
+
+//set views since we set html to an ejs file
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req,res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.info(`listening on port ${port}`);
+})
---
+++ public/js/script.js
@@ -0,0 +1,66 @@
+window.addEventListener('load', () => {
+	const form = document.querySelector("#form");
+	const input = document.querySelector("#input");
+	const list = document.querySelector("#tasks");
+
+    //listening for submit in form
+	form.addEventListener('submit', (e) => {
+		//cancels the event if it is cancelable
+		e.preventDefault();
+
+		let task = input.value;
+
+		//creating a div element and adding a class of 'task' to it
+		let taskElement = document.createElement('div');
+		taskElement.classList.add('task');
+
+		//creating a div element and adding a class of 'content' to it
+		let taskContent = document.createElement('div');
+		taskContent.classList.add('content');
+		
+		taskElement.appendChild(taskContent);
+
+		let taskInput = document.createElement('input');
+		taskInput.classList.add('text');
+		taskInput.type = 'text';
+		taskInput.value = task;
+		taskInput.setAttribute('readonly', 'readonly');
+
+		taskContent.appendChild(taskInput);
+
+		let taskActions = document.createElement('div');
+		taskActions.classList.add('actions');
+		
+		let taskEdit = document.createElement('button');
+		taskEdit.classList.add('edit');
+		taskEdit.innerText = 'Edit';
+
+		let taskDelete = document.createElement('button');
+		taskDelete.classList.add('delete');
+		taskDelete.innerText = 'Delete';
+
+		taskActions.appendChild(taskEdit);
+		taskActions.appendChild(taskDelete);
+
+		taskElement.appendChild(taskActions);
+
+		list.appendChild(taskElement);
+
+		input.value = '';
+
+		taskEdit.addEventListener('click', (e) => {
+			if (taskEdit.innerText.toLowerCase() == "edit") {
+				taskEdit.innerText = "Save";
+				taskInput.removeAttribute("readonly");
+				taskInput.focus();
+			} else {
+				taskEdit.innerText = "Edit";
+				taskInput.setAttribute("readonly", "readonly");
+			}
+		});
+
+		taskDelete.addEventListener('click', (e) => {
+			list.removeChild(taskElement);
+		});
+	});
+});
