293bd2f9fc5c6693246d94bf84bfe149da5f6230
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-06-27 00:51:49 -0400

    even better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //configure creating mysql connection
 const connection = mysql.createConnection({
