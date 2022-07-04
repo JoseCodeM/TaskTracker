e2497954dca098920c9a89b81d3c1e511848ccbf
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-07-04 18:56:38 -0400

    testing for aws amplify

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require("axios");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 //parse app 
 app.use(bodyParser.json());
